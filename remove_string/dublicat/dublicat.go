1f044a7b0789d4ce79632727247ed6b98ca6461e
Sarvarbek Fozilov <t19a0009s@jdu.uz>
2023-07-06 18:05:16 +0500

    get dublicat numbers in array

--- remove_string/dublicat/dublicat.go
+++ remove_string/dublicat/dublicat.go
@@ -2,28 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	// duplicateNumbers := []int{7, 2, 2, 3, 4, 5, 3, 7, 3}
-	duplicateNumbers := []int{4, 3, 4, 3, 4, 5, 3, 7, 3}
-	sorted := []int{}
-	duplicateCount := 0
-	for i := 0; i < len(duplicateNumbers); i++ {
-		for j := 1; j < len(duplicateNumbers)-1; j++ {
-
-			if duplicateNumbers[i] == duplicateNumbers[j] {
-				duplicateCount++
-
-				if duplicateCount > 2 || duplicateCount < 2 duplicateCount == 2 {
-
-					sorted = append(sorted, duplicateNumbers[i])
-					fmt.Println(sorted)
-				} else if  {
+func onlyTwoDuplicates(arr []int) []int {
+	var resultArr []int
+	for i := 0; i < len(arr); i++ {
+		count := 0
+		for j := 0; j < len(arr); j++ {
+			if arr[i] == arr[j] {
+				count++
+			}
+		}
+		if count == 2 {
+			alreadyAdded := false
+			for k := 0; k < len(resultArr); k++ {
+				if resultArr[k] == arr[i] {
+					alreadyAdded = true
 					break
 				}
-
 			}
-
+			if !alreadyAdded {
+				resultArr = append(resultArr, arr[i])
+			}
 		}
-
 	}
+	return resultArr
+}
+
+func main() {
+	fmt.Println(onlyTwoDuplicates([]int{1, 2, 1, 3, 9, 3, 4, 2, 9, 3, 1, 5, 5, 1, 2}))
 }
